1108711af272294ac9be55a33a7d9eeba8f64abe
Kai Horstmann <horstmannkai@hotmail.com>
2019-08-24 14:44:25 +0200

    first commit

---
+++ horOvIp-I2C-Bridge/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * horOvIp-I2C-Bridge.cpp
+ *
+ * Created: 24.08.2019 13:17:18
+ * Author : kai_horstmann
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
